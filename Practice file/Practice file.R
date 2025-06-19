02d122d98ad0a04e4114c5a1710558c01850249c
MAshrafulKabir <ashraful.mission@gmail.com>
2025-06-19 12:08:06 +0600

    Practice.R

--- Practice file/Practice file.R
+++ Practice file/Practice file.R
@@ -1,7 +1,21 @@
-pigeon_beak_length = c(2.2, 1.8, 1.8, 2.05, 2.3, 1.8, 2.05, 1.75)
-x = mean(pigeon_beak_length)
-summary(pigeon_beak_length)
+# Importing the library
+library(ggplot2)
 
-pigeon_tarsus_length = c(10, 2.4, 2.8, 3.25, 6.4, 3.05, 3.25, 3.25)
-y = mean(pigeon_tarsus_length)
-summary(pigeon_tarsus_length)
+# Basic histogram using ggplot2
+ggplot(Before_judging_pigeons, aes(x = Pigeons)) +
+  geom_bar()
+
+# Changing the width of bins
+ggplot(Before_judging_pigeons, aes(x = Pigeons)) +
+  geom_bar(binwidth =1, color = "black")
+
+# changing colors
+ggplot(Before_judging_pigeons, aes(x = Pigeons)) +
+  geom_bar(binwidth =1, color = "black", fill = "lightblue")
+
+# Adding the mean line
+myPlot=ggplot(Before_judging_pigeons, aes(x = Pigeons)) +
+  geom_bar(binwidth =1, color = "black", fill = "white")
+
+myPlot + geom_vline (aes(xintercept = mean(Pigeons)), color = "blue", linetype = "dashed", size = 1)
+
