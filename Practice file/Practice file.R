fd257e8117efb127a6a1445d5e35ae0e67fee54f
MAshrafulKabir <ashraful.mission@gmail.com>
2025-06-19 10:34:32 +0600

    Practice.R

--- Practice file/Practice file.R
+++ Practice file/Practice file.R
@@ -1,3 +1,7 @@
-c = c(2, 3, 4, 5)
-d = c(6, 7, 8)
-summary(c, d)
+pigeon_beak_length = c(2.2, 1.8, 1.8, 2.05, 2.3, 1.8, 2.05, 1.75)
+x = mean(pigeon_beak_length)
+summary(pigeon_beak_length)
+
+pigeon_tarsus_length = c(10, 2.4, 2.8, 3.25, 6.4, 3.05, 3.25, 3.25)
+y = mean(pigeon_tarsus_length)
+summary(pigeon_tarsus_length)
