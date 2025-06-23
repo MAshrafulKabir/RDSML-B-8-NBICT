c6d2264004915165820864f24dba65373e3012db
MAshrafulKabir <ashraful.mission@gmail.com>
2025-06-23 20:55:31 +0600

    Practice_rows_columns

---
+++ Practice file/Practice_rows_columns.R
@@ -0,0 +1,7 @@
+# Matrix in R
+Pigeons_sex = c(male, male, male, male, male)
+Weight = c(350, 355, 400, 356, 358)
+
+# Creating a matrix
+Pigeons_sex_mat = matrix(Pigeons_sex)
+Weight_mat = matrix(weight)
