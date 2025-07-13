090c0f2d4ceb1e639ca6fd8b436679b3a4a02dff
MAshrafulKabir <ashraful.mission@gmail.com>
2025-07-13 16:12:36 +0600

    Basic_pro_theo.R

---
+++ RDSML-Day-21 👉 Basic Probability Theory/Basic_pro_theo.R
@@ -0,0 +1,6 @@
+# Basic probability theory
+# Relative frequency of occurrence
+# Visual explanation
+# Breaking down the probability terminologies
+# Union and intersection in probability
+# Mutually exclusive, independent, and complementary events
