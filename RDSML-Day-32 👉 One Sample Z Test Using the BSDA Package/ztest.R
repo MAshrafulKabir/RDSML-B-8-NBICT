da9c4757c606fa61c715c1dbde1a760bbab5cdb0
MAshrafulKabir <ashraful.mission@gmail.com>
2025-08-21 14:09:08 +0600

    z.test.R

---
+++ RDSML-Day-32 👉 One Sample Z Test Using the BSDA Package/ztest.R
@@ -0,0 +1,12 @@
+# One sample Z test
+# Installing the BSDA package
+# install.packages("BSDA")
+library(BSDA)
+
+# importing the dataset
+dataset = read.csv("Perfume.csv")
+mean(dataset$Machine.1)
+
+z.test(x = dataset$Machine.1, 
+       alternative = "greater", mu = 150, 
+       sigma.x = 2)
