162e73c915df3e2c146bb5f85399b44cd7cddd7b
MAshrafulKabir <ashraful.mission@gmail.com>
2025-10-12 16:52:13 +0600

    Simple_linear_regression.R

---
+++ RDSML-Day-46 👉 Simple Linear Regression in R/Simple_linear_regression.R
@@ -0,0 +1,15 @@
+# Simple linear regression
+# Importing the dataset
+dataset = read.csv("Salary_Data.csv")
+# Splitting the dataset into the training set and test set
+install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+split
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting the simple linear regression model to the training set
+regressor = lm(formula = Salary ~ YearsExperience, 
+               data = training_set)
